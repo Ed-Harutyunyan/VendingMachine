08b8e7ecd12dfb2126162fb97e474581317b4908
Ed-Harutyunyan <edgar.harutiunyan@icloud.com>
2021-12-01 14:06:29 +0400

    Added list maker

--- main_folder/tester.js
+++ main_folder/tester.js
@@ -4,18 +4,11 @@ const data = require('./products.json');
 
 const options = reader.question("Please enter an option: ")
 
-const names = Object.data(products)
-
-console.log(data.products[names].name)
-
-
 // So if you want to see the list of products this vending machine has, you write "list" and you get the names of products available.
 //Later when figure out the way to print the names, will also add the prices 
 
  if (options == "list") {
-    let result
-    for (let i = 0; i < data.length; i++) {
-        result += data.products[i].name
+    for (let i = 0; i < data.products.length; i++) {
+        console.log(JSON.stringify(data['products'][i]['id']) + "\t" + JSON.stringify(data['products'][i]['name']) + "\t" + JSON.stringify(data['products'][i]['price'])) + "\n"
     }
-    console.log(result);
 }
