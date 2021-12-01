9de8654d72eca49c704b3a3965f2aa41f1848c00
Ed-Harutyunyan <edgar.harutiunyan@icloud.com>
2021-12-01 15:47:47 +0400

    setTimeOut aka loading for machine, throws an unknown error

--- main_folder/tester.js
+++ main_folder/tester.js
@@ -5,20 +5,25 @@ const data = require('./products.json');
 const options = reader.question("Please enter an option: ")
 
 // So if you want to see the list of products this vending machine has, you write "list" and you get the names of products available.
-//Later when figure out the way to print the names, will also add the prices 
 
  if (options == "list") {
         console.table(data.products)
 }
 
+//gives the product after asking what you want
 let chosenProductByCustomer = reader.question("Please enter a products name: ")
 
-if (chosenProductByCustomer == "Coca Cola") {
-    console.log(`Here's your ${chosenProductByCustomer} 🧃`);
-} else if (chosenProductByCustomer == "Water") {
-    console.log(`Here's your ${chosenProductByCustomer} 💧`);
-} else if (chosenProductByCustomer == "Snickers") {
-    console.log(`Here's your ${chosenProductByCustomer} 🍫`);
-} else {
-    console.log(`Here's your ${chosenProductByCustomer}`);
+function giveNeededProduct (val) {
+
+    if (chosenProductByCustomer == "Coca Cola") {
+        console.log(`Here's your ${chosenProductByCustomer} 🧃`);
+    } else if (chosenProductByCustomer == "Water") {
+        console.log(`Here's your ${chosenProductByCustomer} 💧`);
+    } else if (chosenProductByCustomer == "Snickers") {
+        console.log(`Here's your ${chosenProductByCustomer} 🍫`);
+    } else {
+        console.log(`Here's your ${chosenProductByCustomer}`);
+    }
 }
+
+setTimeout(giveNeededProduct(chosenProductByCustomer), 1000)
